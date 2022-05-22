bc0491f372f183ab0be115e3f9f0a5db3fd43594
couvq <quentincouvelaire@gmail.com>
2022-05-22 10:02:53 -0400

    added a searchbar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import SearchBar from './SearchBar';
 
 const NavBar = () => {
 
 
   return (
-    <div>NavBar</div>
+    <>
+        <SearchBar />
+    </>
   );
 }
 
