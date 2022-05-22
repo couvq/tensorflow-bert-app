19d090917057d6808d3f036c6e334df1d6cb3a74
couvq <quentincouvelaire@gmail.com>
2022-05-22 11:10:20 -0400

    setup refs and state hooks

--- src/App.js
+++ src/App.js
@@ -6,7 +6,21 @@ import * as tf from '@tensorflow/tfjs';
 import * as qna from '@tensorflow-models/qna';
 import Loader from 'react-loader';
 
+import { useState, useEffect, useRef } from 'react';
+
 const App = () => {
+  const [question, setQuestion] = useState('');
+  const [answer, setAnswer] = useState();
+  const [model, setModel] = useState();
+
+  const passageRef = useRef(null);
+
+  const getQuestion = (e) => {
+    if(e.which === 13) { // enter key was pressed
+
+    }
+  }
+
 
 
   return (
