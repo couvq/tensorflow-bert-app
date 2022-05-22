7370d8d288c6a92f8d458dcdb33523d17cc51fa6
couvq <quentincouvelaire@gmail.com>
2022-05-22 10:54:54 -0400

    imported tensorflow, qna model, and Loader dependencies into App component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import "./index.css";
 import NavBar from './components/NavBar';
 
+import * as tf from '@tensorflow/tfjs';
+import * as qna from '@tensorflow-models/qna';
+import Loader from 'react-loader';
+
 const App = () => {
 
 
