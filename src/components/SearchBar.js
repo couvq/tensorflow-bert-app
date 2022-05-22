6f531f46c663f75f3a8b94d33f55e17679698496
couvq <quentincouvelaire@gmail.com>
2022-05-22 13:12:27 -0400

    search now clears upon hitting clear button icon

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 // import MicIcon from '@mui/icons-material/Mic';
 import ClearIcon from '@mui/icons-material/Clear';
@@ -6,21 +7,31 @@ import { InputBase, Paper, IconButton } from '@mui/material';
 
 const SearchBar = ({ disabled, answerQuestion }) => {
 
+    const [input, setInput] = useState('');
+
 
     return (
         <>
             <Paper>
                 <IconButton
+                disabled
                 >
                     <SearchIcon />
                 </IconButton>
                 <InputBase
                     placeholder='Ask BERT a question...'
                     onKeyPress={answerQuestion}
+                    onChange={(e) => {
+                        setInput(e.target.value)
+                    }}
                     { ...disabled ? 'disabled' : '' }
+                    value={input}
                 />
                 <IconButton
                     { ...disabled ? 'disabled' : '' }
+                    onClick={()=> {
+                        setInput('');
+                    }}
                 >
                     <ClearIcon />
                 </IconButton>
