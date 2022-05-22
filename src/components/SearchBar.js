4fe746ef9b6fb236ce7743b4255356824dca33d5
couvq <quentincouvelaire@gmail.com>
2022-05-22 10:41:56 -0400

    started impl for searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import SearchIcon from '@mui/icons-material/Search';
+import MicIcon from '@mui/icons-material/Mic';
+import ClearIcon from '@mui/icons-material/Clear';
+import { InputBase, Paper, IconButton } from '@mui/material';
 
 const SearchBar = () => {
 
 
   return (
-    <div>SearchBar</div>
+    <>
+        <Paper>
+            <IconButton>
+                <SearchIcon />
+            </IconButton>
+            <InputBase />
+            <IconButton>
+                <MicIcon />
+            </IconButton>
+        </Paper>
+    </>
   );
 }
 
